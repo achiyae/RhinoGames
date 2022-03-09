8bb7ce10b22c68e32c0a483e41a4472baa298a52
Achiya Elyasaf <10044875+eggsterino@users.noreply.github.com>
2022-03-09 10:52:54 +0200

    initial commit to issue 1192

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import org.mozilla.javascript.Context;
+import org.mozilla.javascript.NativeObject;
 import org.mozilla.javascript.tools.shell.Global;
 
 import java.io.BufferedReader;
@@ -6,14 +7,25 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
 public class Main {
   public static void main(String[] args) {
     Context cx = Context.enter();
     try {
-      Global global = new Global(cx);
-      String script = getResourceScript();
-      Object result = cx.evaluateString(global, script, "code.js", 1, null);
+      var scope = cx.initStandardObjects();
+      String script =
+          "var a1 = {a:'a'};\n" +
+              "var a2 = {a:'a'};\n" +
+              "var a3 = {a:'a'};\n" +
+              "Object.is(a1,a2);";
+      cx.evaluateString(scope, script, "code.js", 1, null);
+      var a1 = (NativeObject) Context.jsToJava(scope.get("a1"), NativeObject.class);
+      var a2 = (NativeObject) Context.jsToJava(scope.get("a2"), NativeObject.class);
+      var a3 = (NativeObject) Context.jsToJava(scope.get("a3"), NativeObject.class);
+      System.out.println(a1.equals(a2));
+      System.out.println(a2.equals(a3));
+      System.out.println(a2.equals(a1));
     } finally {
       Context.exit();
     }
@@ -22,8 +34,8 @@ public class Main {
   private static String getResourceScript() {
     String script;
     try (InputStream resource = Thread.currentThread().getContextClassLoader().getResourceAsStream("code.js");
-       InputStreamReader streamReader = new InputStreamReader(resource, StandardCharsets.UTF_8);
-       BufferedReader br = new BufferedReader(streamReader)) {
+         InputStreamReader streamReader = new InputStreamReader(resource, StandardCharsets.UTF_8);
+         BufferedReader br = new BufferedReader(streamReader)) {
       StringBuilder sb = new StringBuilder();
       String line;
       try {
