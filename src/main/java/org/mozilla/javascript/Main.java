284f14881d9242735ee25774cf4f0af588159a8c
Achiya Elyasaf <10044875+eggsterino@users.noreply.github.com>
2022-03-10 16:43:30 +0200

    A minimal use case for issue 1192

--- src/main/java/org/mozilla/javascript/Main.java
+++ src/main/java/org/mozilla/javascript/Main.java
@@ -2,19 +2,23 @@ package org.mozilla.javascript;
 
 public class Main {
   public static void main(String[] args) {
-    var o1 = makeObject1();
-    var o2 = makeObject2();
+    testTransitive();
+  }
+
+  public static void testTransitive() {
+    var o1 = makeTransitive1();
+    var o2 = makeTransitive2();
     System.out.println(new EqualObjectGraphs().equalGraphs(o1, o2)); // prints false
   }
 
-  private static Object makeObject1() {
-    var o1 = new Object[]{"3"};
-    var o2 = new Object[]{"3"};
+  private static Object makeTransitive1() {
+    Object o1 = new Object[]{"3"};
+    Object o2 = new Object[]{"3"};
     return new Object[]{o1, o2};
   }
 
-  private static Object makeObject2() {
-    var o1 = new Object[]{"3"};
+  private static Object makeTransitive2() {
+    Object o1 = new Object[]{"3"};
     return new Object[]{o1, o1};
   }
 }
